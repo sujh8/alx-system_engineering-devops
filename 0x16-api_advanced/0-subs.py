9825f0ec228ae6b5c31fc69f72557c1e3120cb26
sujh8 <suhajhm@yahoo.com>
2024-05-07 10:18:21 +0000

    py

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -10,7 +10,7 @@ def number_of_subscribers(subreddit):
     '''
         returns the number of subscribers for a given subreddit
     '''
-    user = {'User-Agent': 'Julietobiyor'}
+    user = {'User-Agent': 'sujh8'}
     url = requests.get('https://www.reddit.com/r/{}/about.json'
                        .format(subreddit), headers=user).json()
     try:
